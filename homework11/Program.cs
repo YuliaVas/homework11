8cb99d46bbfbaa1703529b66521c58cf32bca874
YuliyaVas <yulenka2596@gmail.com>
2022-10-09 01:31:29 +0300

    Добавьте файлы проекта.

---
+++ homework11/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace homework11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationDictionary <string, string> dict = new ApplicationDictionary<string, string>();
+            dict.Add("flower", "цветок");
+            dict.Add("table", "стол");
+            dict.Add("tree", "дерево");
+            dict.ListOfKey();
+            dict.Remove("table");
+            dict.ListOfKey();
+            Console.WriteLine(dict.GetValueByKey("tree"));
+
+        }
+    }
+}
